831a2c2988c914c8656454d2a85455f45e7a1ae1
Moeed Khalid <46730129+M-MoeedKhalid@users.noreply.github.com>
2021-02-02 03:21:15 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,52 @@
+import nltk
+nltk.download('words')
+from nltk.corpus import words
+from nltk import edit_distance
+
+# file1 = open("data/FAWTHROP1DAT.643")
+
+# Using readline()
+file1 = open("data/FAWTHROP1DAT.643")
+count = 0
+mistakes = dict()
+
+while True:
+    count += 1
+    line = file1.readline()
+    if not line:
+        break
+    mistake_pair = line.strip().split()
+    mistakes[mistake_pair[1]] = mistake_pair[0]
+
+file1.close()
+
+word_list = words.words()
+common_word_list = []
+
+# for word in word_list:
+#     if wordnet.synsets(word):
+#         common_word_list.append(word)
+#     else:
+#         pass
+
+result = dict()
+positions = []
+
+for index, (key, value) in enumerate(mistakes.items()):
+    if index % 27 == 0:
+        print(key, value)
+        key = 'ABBATOIR'
+        value = 'ABATTOIR'
+        for word in word_list:
+            distance = edit_distance(key.upper(), word.upper())
+            result[word] = distance
+            print(str(word) + ": " + str(distance))
+            y = 2
+        sorted_result = dict(sorted(result.items(), key=lambda x: x[1]))
+        result2 = {k: sorted_result[k] for k in list(sorted_result)[:10]}
+        positions.append(result2.get(str(value).lower()))
+        print("For the mistake " + key.lower() + ", the correct word " + value.lower()
+              + " was found at " + str(result2.get(str(value).lower())))
+        result = dict()
+
+ewr = 4
