29553a3e4636d0f40f471e18c854a0dcd01c115d
Hossein Fani <hosseinfani@gmail.com>
2021-02-07 21:47:42 -0500

    calculating success_k using pytrec-eval lib

--- autospell_corrector.py
+++ autospell_corrector.py
@@ -1,27 +1,35 @@
+import random
+random.seed(1234)
+import json
 import nltk
 nltk.download('words')
 from nltk.corpus import words
 from nltk import edit_distance
 
+import pytrec_eval
+
+
+#D: Dictionary of words
+#C: Corpus of misspelled words paired with the word intended (correct word)
+#N: top-N similar words
+D = words.words()
+N = 10
+
 # file1 = open("data/FAWTHROP1DAT.643")
 
 # Using readline()
-file1 = open("data/FAWTHROP1DAT.643")
+filename = 'data/FAWTHROP1DAT.643'
+C_file = open(filename)
 count = 0
 mistakes = dict()
-
 while True:
     count += 1
-    line = file1.readline()
+    line = C_file.readline()
     if not line:
         break
     mistake_pair = line.strip().split()
     mistakes[mistake_pair[1]] = mistake_pair[0]
-
-file1.close()
-
-word_list = words.words()
-common_word_list = []
+C_file.close()
 
 # for word in word_list:
 #     if wordnet.synsets(word):
@@ -29,24 +37,47 @@ common_word_list = []
 #     else:
 #         pass
 
-result = dict()
-positions = []
+mistakes_samples = mistakes
+#mistakes_samples = dict()
+#random selection of the list for lower the time complexity
+#for i in range(5):
+#    k, v = random.choice(list(mistakes.items()))
+#    mistakes_samples[k] = v
 
-for index, (key, value) in enumerate(mistakes.items()):
-    if index % 27 == 0:
-        print(key, value)
-        key = 'ABBATOIR'
-        value = 'ABATTOIR'
-        for word in word_list:
-            distance = edit_distance(key.upper(), word.upper())
-            result[word] = distance
-            print(str(word) + ": " + str(distance))
-            y = 2
-        sorted_result = dict(sorted(result.items(), key=lambda x: x[1]))
-        result2 = {k: sorted_result[k] for k in list(sorted_result)[:10]}
-        positions.append(result2.get(str(value).lower()))
-        print("For the mistake " + key.lower() + ", the correct word " + value.lower()
-              + " was found at " + str(result2.get(str(value).lower())))
-        result = dict()
+qrel = dict()
+qret = dict()
 
-ewr = 4
+topN = dict()
+for i, (misspelled, correct) in enumerate(mistakes_samples.items()):
+    misspelled = misspelled.lower()
+    correct = correct.lower()
+    result = dict()
+    for word in D:
+        word = word.lower()
+        distance = edit_distance(misspelled, word)
+        result[word] = distance
+        #print(str(word) + ": " + str(distance))
+
+    sorted_result = dict(sorted(result.items(), key=lambda x: x[1]))
+    topN[misspelled] = {k: (-1) * sorted_result[k] for k in list(sorted_result)[:N]}
+
+    print(f'{misspelled, correct}')
+    for j, (k, v) in enumerate(topN[misspelled].items()):
+        asterisk = '*' if k.lower() == correct.lower() else ''
+        print(f'{k}: {v} {asterisk}')
+
+    #prepare for metric calculation
+    qrel[misspelled] = {correct: 1}
+    qret[misspelled] = topN[misspelled]
+    #print(qrel)
+    #print(qret)
+
+#qret['wierd'] = {'weirddiii': -1,'we': -2,'wei': -3,'weir': -4,'weirddii': -5,'weir3ddii': -6,'we33irddii': -7,'weird': -8}
+#{"wierd": {"success_1": 0.0,"success_5": 0.0,"success_10": 1.0}
+metrics = {'success_1', 'success_5', 'success_10'}
+evaluator = pytrec_eval.RelevanceEvaluator(qrel, metrics)
+results = evaluator.evaluate(qret)
+avg_result = dict()
+for m in metrics:
+    avg_result[m] = sum([v[m] for (k, v) in results.items()])/len(results)
+print(f'{filename} => {avg_result}')
